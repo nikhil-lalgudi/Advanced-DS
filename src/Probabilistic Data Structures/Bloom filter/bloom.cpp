573174ad46ccfea121dd7d2b8144cd928805fc4e
Nikhil Lalgudi Vaidyanathan <144494028+nikhil-lalgudi@users.noreply.github.com>
2024-09-20 04:17:45 -0400

    starting with bloom/cuckoo

---
+++ src/Probabilistic Data Structures/Bloom filter/bloom.cpp
@@ -0,0 +1,9 @@
+#ifndef BLOOM_H
+#define BLOOM_H
+
+#include <algorithm>
+#include <assert.h>
+#include <sstream>
+#include <climits>
+
+using namespace std;
---
+++ src/Probabilistic Data Structures/Cuckoo filter/cuckoo.h
@@ -0,0 +1,5 @@
+#ifndef CUCKOO_H
+#define CUCKOO_H
+
+#include <assert.h>
+#include <algorithm>
